dd4560776549d2bcf54577bbca0df81ae1bdfdf4
Pedro Gandra <pedro.gandra.andrade@gmail.com>
2025-06-28 09:03:44 -0300

    Descriptografar v2.0 Entrada de MAX_KEY e nivel de precisao maximos. Barra de progresso exibida para decodificações que podem levar mais tempo.

--- src/descriptografar.c
+++ src/descriptografar.c
@@ -7,29 +7,85 @@
 #include <time.h>
 
 #define MAX_LEN 30
-#define MAX_KEY 10
 #define ALF_LEN 26
 #define MAX_TXT 10000
 #define TOTAL_BI 393
 #define TOTAL_WORDS 245366
-#define MAX_CESAR 5
+#define MAX_PRECISAO 5
 #define MIN_COBERTURA 0.9
+#define BI_ALVO 0.1
 
 char allPalavras[TOTAL_WORDS][MAX_LEN];
 int qtdePalavras[26];
 char bigramasRaros[TOTAL_BI][2];
-float freqAlvo;
-int cesarAlvo = 1;
-float bigramasAlvo;
+int precisaoAlvo = -1;
+int maxKey = -1;
+unsigned long long cTotal, cAtual = 0, cUltimo = 0;
 
 double frequencias[ALF_LEN] = {0};
 int cVogais = 0, cBigramas = 0, cCobertura = 0;
-double tTotal = 0, tPre = 0, tVogais = 0, tBigramas = 0, tCobertura = 0;
+double tTotal = 0, tVogais = 0, tBigramas = 0, tCobertura = 0;
 
 typedef struct {
     char l;
     double valor;   
 } probLetra;
+
+void formatarTempo(double segundos, char* buffer, size_t tam) {
+    if (segundos < 60.0) {
+        snprintf(buffer, tam, "%.1fs", segundos);
+    } else if (segundos < 3600.0) {
+        int min = (int)(segundos / 60);
+        int sec = (int)(segundos) % 60;
+        snprintf(buffer, tam, "%dm %ds", min, sec);
+    } else if (segundos < 86400.0) {
+        int h = (int)(segundos / 3600);
+        int m = ((int)segundos % 3600) / 60;
+        snprintf(buffer, tam, "%dh %dm", h, m);
+    } else {
+        int d = (int)(segundos / 86400);
+        int h = ((int)segundos % 86400) / 3600;
+        snprintf(buffer, tam, "%dd %dh", d, h);
+    }
+}
+
+void atualizarProgresso() {
+    static clock_t inicio = 0;
+    static clock_t ultimaAtualizacao = 0;
+    cUltimo = cAtual;
+
+    if (inicio == 0) {
+        inicio = clock();
+        ultimaAtualizacao = inicio;
+        return;
+    }
+
+    clock_t agora = clock();
+    double decorrido = (double)(agora - inicio) / CLOCKS_PER_SEC;
+    double desdeUltima = (double)(agora - ultimaAtualizacao) / CLOCKS_PER_SEC;
+
+    if (desdeUltima < 1.0 || cAtual == 0) return;
+
+    double progresso = (double)cAtual / (double)cTotal;
+    if (progresso <= 0.0) return;
+
+    double tempoEstimadoTotal = decorrido / progresso;
+    double restante = tempoEstimadoTotal - decorrido;
+
+    if (tempoEstimadoTotal < 5.0) return;
+
+    char bufDecorrido[32];
+    char bufRestante[32];
+
+    formatarTempo(decorrido, bufDecorrido, sizeof(bufDecorrido));
+    formatarTempo(restante, bufRestante, sizeof(bufRestante));
+
+    fprintf(stderr, "\rProgresso: %5.1f%% | Decorrido: %s | Tempo maximo restante: %s",
+            progresso * 100.0, bufDecorrido, bufRestante);
+    fflush(stderr);
+
+    ultimaAtualizacao = agora;
+}
 
 void copyWordsAndFrequency(FILE* palavras, char w[][MAX_LEN]) {
     int i = 0, l = 0;
@@ -148,7 +204,7 @@ bool validarBigramas(char str[]) {
         char bi[] = {str[i-1], str[i]};
         if(bigramaInList(bi)) {
             cont++;
-            if(cont >= (size-1)*bigramasAlvo) {
+            if(cont >= (size-1)*BI_ALVO) {
                 fim = clock();
                 tBigramas += (double)(fim - inicio) / CLOCKS_PER_SEC;
                 return false;
@@ -253,14 +309,14 @@ void decode(char new[], char txt[], char key[]) {
     }
 }
 
-void inserir(probLetra prov[], double sim, int l) {
+void inserir(int precisao, probLetra prov[], double sim, int l) {
     probLetra novo;
     novo.l = l + 'a';
     novo.valor = sim;
 
-    for (int i = 0; i < cesarAlvo; i++) {
+    for (int i = 0; i < precisao; i++) {
         if (prov[i].valor == 0 || sim > prov[i].valor) {
-            for (int j = cesarAlvo - 1; j > i; j--) {
+            for (int j = precisao - 1; j > i; j--) {
                 prov[j] = prov[j - 1];
             }
             prov[i] = novo;
@@ -269,35 +325,42 @@ void inserir(probLetra prov[], double sim, int l) {
     }
 }
 
-bool gerarCombinacoes(int pos, int sizeKey, char chave[], char letras[][cesarAlvo], char new[], char txt[]) {
-    if (pos == sizeKey) {
-        chave[pos] = '\0';
-        decode(new, txt, chave);
-        if(validarVogais(new)) {
-            if(validarBigramas(new)) {
-                if(coberturaPalavras(new) >= MIN_COBERTURA) {
-                    printf("%s - %s\n", new, chave);
-                    return true;
-                } else
-                    cCobertura++;
-            } else
-                cBigramas++;
-        } else
-            cVogais++;
-        return false;
-    }
-
-    for (int i = 0; i < cesarAlvo; i++) {
-        chave[pos] = letras[pos][i];
-        if (gerarCombinacoes(pos + 1, sizeKey, chave, letras, new, txt)) {
-            return true;
+bool gerarCombinacoes(int precisao, int sizeKey, char letras[][precisao], char chave[], char new[], char txt[]) {
+    int indices[100] = {0}; // Posição atual em cada coluna de letras
+    int pos = 0;
+    while (pos >= 0) {
+        if (indices[pos] < precisao) {
+            chave[pos] = letras[pos][indices[pos]];
+            indices[pos]++;
+            if (pos == sizeKey - 1) {
+                chave[sizeKey] = '\0';
+                decode(new, txt, chave);
+                if (validarVogais(new)) {
+                    if (validarBigramas(new)) {
+                        if (coberturaPalavras(new) >= MIN_COBERTURA) {
+                            fprintf(stderr, "\r%*s\r", 80, "");
+                            printf("**************************************************************\n\nTEXTO DECODIFICADO\n");
+                            printf("Texto original: %s\nChave: %s\n", new, chave);
+                            printf("\n**************************************************************\n\n");
+                            return true;
+                        } else cCobertura++;
+                    } else cBigramas++;
+                } else cVogais++;
+                cAtual++;
+                if (cAtual - cUltimo > 100000)
+                    atualizarProgresso();
+            } else {
+                pos++;
+                indices[pos] = 0;
+            }
+        } else {
+            pos--;
         }
     }
-
     return false;
 }
 
-void letrasProvaveis(char txt[], char letras[][cesarAlvo], int sizeKey) {
+void letrasProvaveis(int precisao, char txt[], char letras[][precisao], int sizeKey) {
     for(int i = 0; i < sizeKey; i++) {
         char coluna[MAX_TXT];
         int cont = 0;
@@ -307,27 +370,18 @@ void letrasProvaveis(char txt[], char letras[][cesarAlvo], int sizeKey) {
         }
         coluna[cont] = '\0';
         char novo[cont+1];
-        probLetra provaveis[MAX_CESAR] = {0};
+        probLetra provaveis[MAX_PRECISAO] = {0};
         for(int j = 0; j < ALF_LEN; j++) {
             strcpy(novo, coluna);
             for(int k = 0; k < cont; k++) {
                 novo[k] = deslocar(novo[k], j+'a');
             }
             double sim = calcularSimilaridade(novo);
-            inserir(provaveis, sim, j);
+            inserir(precisao, provaveis, sim, j);
         }
-        for(int j = 0; j < cesarAlvo; j++) {
+        for(int j = 0; j < precisao; j++) {
             letras[i][j] = provaveis[j].l;
         }
-    }
-}
-
-void printProvaveis(char letras[][cesarAlvo], int sizeKey) {
-    for(int i = 0; i < sizeKey; i++) {
-        for(int j = 0; j < cesarAlvo; j++) {
-            printf("%c, ", letras[i][j]);
-        }
-        printf("\n");
     }
 }
 
@@ -335,60 +389,67 @@ bool descriptografar(char txt[]) {
     int sizeKey;
     char new[MAX_TXT];
     strcpy(new, txt);
-    for(sizeKey = 1; sizeKey <= MAX_KEY; sizeKey++) {
-        char letras[sizeKey][cesarAlvo];
-        char key[sizeKey+1];
-        letrasProvaveis(txt, letras, sizeKey);
-        if(gerarCombinacoes(0, sizeKey, key, letras, new, txt))
-            return true;
+    int prec = 1;
+    while(prec <= precisaoAlvo) {
+        for(sizeKey = 1; sizeKey <= maxKey; sizeKey++) {
+            char letras[sizeKey][prec];
+            char key[sizeKey+1];
+            letrasProvaveis(prec, txt, letras, sizeKey);
+            if(gerarCombinacoes(prec, sizeKey, letras, key, new, txt)) {
+                printf("Precisao necessaria: %d\n\n", prec);
+                return true;
+            }
+        }
+        prec++;
     }
     return false;
 }
 
+int calcularPrecisao(int maxKey) {
+    if(maxKey > 22)
+        return 1;
+    if(maxKey > 14)
+        return 2;
+    if(maxKey > 11)
+        return 3;
+    if(maxKey > 9)
+        return 4;
+    return 5;
+}
+
 int main() {
-    clock_t inicio, fim;
-    inicio = clock();
-    freqAlvo = 0.85;
-    cesarAlvo = 1;
-    bigramasAlvo = 0.10;
     FILE* listaMinusculas = fopen("resources/lista-minusculas.txt", "r");
     FILE* textoCifrado = fopen("resources/texto-cifrado.txt", "r");
     copyWordsAndFrequency(listaMinusculas, allPalavras);
     copyBigramas(bigramasRaros);
     char txt[MAX_TXT];
     limparTexto(textoCifrado, txt);
-    fim = clock();
-    tPre = (double)(fim - inicio) / CLOCKS_PER_SEC;
+
+    while(maxKey <= 0) {
+        printf("Qual o tamanho maximo da chave a ser testada: ");
+        scanf("%d", &maxKey);
+    }
+    while(precisaoAlvo < 0 || precisaoAlvo > 5) {
+        printf("Nivel maximo de precisao (1 a 5)? Escolha 0 caso queira que o programa escolha para voce (recomendado): ");
+        scanf("%d", &precisaoAlvo);
+    }
+    if(precisaoAlvo == 0) {
+        precisaoAlvo = calcularPrecisao(maxKey);
+        printf("Precisao maxima escolhida: %d\n", precisaoAlvo);
+    }
+    cTotal = pow(precisaoAlvo, maxKey);
+
+    clock_t inicio, fim;
+    inicio = clock();
     printf("\nDESCRIPTOGRAFIA INICIADA\n\n");
     printf("Texto referencia:\n");
-    printf("%s\n", txt);
-    while(cesarAlvo <= MAX_CESAR) {
-        printf("\nTentando descriptografia com precisao de nivel %d:\n", cesarAlvo);
-        if(descriptografar(txt))
-            break;
-        cesarAlvo++;
-        printf("Nenhum texto possivel\n");
-    }
-    printf("\n");
-    //printf("%.4f\n", calcularSimilaridade("eaiqualvaiseragoratuvaiterqueescolheroueuouacachacasedecidebebe"));
-    //printf("%.4f\n", coberturaPalavras("eaequalvaisaragoraturaiterqueascolheroqeuouacacdacasedecedebebe"));
+    printf("%s\n\n", txt);
+    bool sucesso = false;
+    if(!descriptografar(txt))
+        printf("Nao foi possivel decodificar o texto, a chave usada eh maior do que o maximo informado ou a proporcao texto/chave nao eh grande o suficiente para a precisao utilizada!\n\n");
     fclose(listaMinusculas);
     fclose(textoCifrado);
     fim = clock();
     tTotal = (double)(fim - inicio) / CLOCKS_PER_SEC;
-
-    printf("ESTATISTICAS\n\n");
-
-    printf("Tempo gasto:\n");
-    printf("Total: %.4f segundos\n", tTotal);
-    printf("Pre-processamento: %.4f segundos\n", tPre);
-    printf("Analisando vogais: %.4f segundos\n", tVogais);
-    printf("Analisando bigramas: %.4f segundos\n", tBigramas);
-    printf("Checando na lista de palavras: %.4f segundos\n", tCobertura);
-
-    printf("\nEliminacao de possibilidades:\n");
-    printf("Poucas vogais: %d\n", cVogais);
-    printf("Bigramas raros: %d\n", cBigramas);
-    printf("Sem palavras reais: %d\n", cCobertura);
     return 0;
 }
